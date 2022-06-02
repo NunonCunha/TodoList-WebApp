d3f56959eaa554db6c1ab0b2cf260366427c73d5
Nuno Cunha <67425493+NunonCunha@users.noreply.github.com>
2022-06-03 00:54:28 +0100

    create task

--- static/login.js
+++ static/login.js
@@ -1,20 +1,19 @@
-function tratar_recv(data){
-    if (data['error'] == true){
-        $("#icon").addClass("fa-solid fa-circle-exclamation");
-        $("#msgErro").text("Nome do utilizador ou palavra-passe inválido!");
-    }
-    else{
-        $(location).prop('href', data['redirect']);
-    }
+function tratar_recv(data) {
+  if (data["error"] == true) {
+    $("#icon").addClass("fa-solid fa-circle-exclamation");
+    $("#msgErro").text("Nome do utilizador ou palavra-passe inválido!");
+  } else {
+    $(location).prop("href", data["redirect"]);
+  }
 }
 
-function submeter_form(){
-    valor_usr = $('input[name="email"]').val();
-    valor_pwd = $('input[name="password"]').val();
-    json_send = {email: valor_usr, password: valor_pwd};
-    $.post($SCRIPT_ROOT + '/login', json_send, tratar_recv, 'json');
+function submeter_form() {
+  valor_usr = $('input[name="email"]').val();
+  valor_pwd = $('input[name="password"]').val();
+  json_send = { email: valor_usr, password: valor_pwd };
+  $.post($SCRIPT_ROOT + "/login", json_send, tratar_recv, "json");
 }
 
-$(function() {
-    $('#submit').click(submeter_form);
+$(function () {
+  $("#submit").click(submeter_form);
 });
